d3478ffba12dfa5b836354f1c9b7c6b92b6ea212
Lucas Forsythe <lucasforsythe2004@gmail.com>
2026-09-09 15:21:36 -0400

    Checkpoint 3
    Added prefabs and reviewed execution order

--- Assets/Scripts/FlyAtPlayer.cs
+++ Assets/Scripts/FlyAtPlayer.cs
@@ -6,6 +6,11 @@ public class FlyAtPlayer : MonoBehaviour
     [SerializeField] Transform player;
     Vector3 playerPosition;
 
+
+    void Awake()
+    {
+        gameObject.SetActive(false);
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
